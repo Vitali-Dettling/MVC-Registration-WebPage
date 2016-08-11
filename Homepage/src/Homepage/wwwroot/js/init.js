2e05ec078f2541bc44d7788baefb79aee0ae708d
Vitali Dettling <vitali.dettling@hotmail.com>
2016-08-11 16:37:45 +0200

    Basic implementation of the Panna homepage: - login form - registration form No logic or API call has been implemented yet.

---
+++ Homepage/src/Homepage/wwwroot/js/init.js
@@ -0,0 +1,19 @@
+﻿
+//header index
+$("#menu_index").html("<ul>" +
+              "<li class=\"active\"><a href=\"index.html\">Home</a></li> " +
+              "<li class=\"active\"><a href=\"html/register.html\">Sign up</a></li> " +
+              "<li class=\"active\"><a href=\"html/about.html\">About</a></li>" +
+              "</ul>")
+
+//header html folder
+$("#menu_html_folder").html("<ul>" +
+              "<li class=\"active\"><a href=\"../index.html\">Home</a></li> " +
+              "<li class=\"active\"><a href=\"register.html\">Sign up</a></li> " +
+              "<li class=\"active\"><a href=\"about.html\">About</a></li>" +
+              "</ul>")
+
+//footer
+$("#footer").html("<a>&copy; 2016 Wirtschaftsingenieurbüro J.C. Fiebig</a>");
+
+
